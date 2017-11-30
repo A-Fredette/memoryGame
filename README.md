20fa9b3e9ada078393861883de2a7e7da529d83b
A-Fredette <andrew.fredett@gmail.com>
2017-11-29 20:44:59 -0800

    final commit before rubric check and cleanup

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # memoryGame
-Memory game for Udacity's Front End Development
+Memory game for Udacity's Front End Development Nanodegree program. This project showcases intermediate javascript skills and includes several popular libraries, including jQuery, animcate.css and animatedModal.js. 
--- js/app.js
+++ js/app.js
@@ -19,8 +19,8 @@ var cardOrder = [
 
  ],
 openCards = [],
-move = 11,
-matchedCards = 14,
+move = 0,
+matchedCards = 0,
 time,
 duration;
 
@@ -30,7 +30,6 @@ duration;
  *   - loop through each card and create its HTML
  *   - add each card's HTML to the page
  */
-
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
   var currentIndex = array.length, temporaryValue, randomIndex;
@@ -50,20 +49,10 @@ function shuffle(array) {
   return array;
 }
 
-function addModal() {
-  $('#lean_overlay').append('<h1 class= "overlay-text overlay-heading"><b>Congratulations</b></h1>')
-    .append('<h3 class= "overlay-text rating">You Won!</h3>')
-    .append('<h3 class= "overlay-text rating">Your rating: </h3>')
-    .append('<h3 class= "overlay-text time">Completion Time: '+ duration + ' Seconds</h3>')
-    .append('<h2 class= "overlay-text play-again">Play Again?</h2>');
-    $('.play-again').after('<button class= "overlay-text play-button">Play Again</button>');
-    $('.play-button').after('<button class= "modal-close">Close</button>');
-
-  $('.fa-star').each(function() {
-    $('rating').append('<li><i class="fa fa-star"></i></li>');
-  });
-}
-
+/*
+ * Reads the move variable value and displays number of user moves
+ *   - Each time a user reaches 10 moves, they lose a rating (display star removed)
+ */
 function trackScore() {
   if (move === 1) {
     $('.moves').html(move+" Move");
@@ -73,40 +62,63 @@ function trackScore() {
   }
 
   if (move % 10 === 0) {
-    $('.stars').children(':first').remove();
+    $('.stars').children(':first').addClass('animated pulse');
+
+    setTimeout(function() {
+      $('.stars').children(':first').remove();
+      }, 1000);
   }
 }
 
+/*
+ * If there is a match, set the 'card' class to .match
+ *   - Remove .clicked
+ *   - Once all cards are matched (matchedCards = 16), produce the modal
+ *   - Empty array on clicked cards
+ *   - Update score (moves + rating)
+ *   - Animation courtesy of aninimate.css: https://daneden.github.io/animate.css/
+ */ 
 function match(array, item) {
-  $("."+item).parent().addClass('match').removeClass('clicked');
+  $("."+item).parent().addClass('match bounce').removeClass('clicked');
+
+  setTimeout(function() {
+      $('.'+value).parent().css({'transition': '', 'transform': ''})
+      .removeClass('clicked');
+      }, 1600);
 
   array.splice(0, 2);
 
   matchedCards = matchedCards + 2;
 
   if (matchedCards === 16) {
-    $(document).leanModal({ top : 200, overlay : 0.9, closeButton: ".modal-close" });
     addModal();
+
+    setTimeout(function() {
+      restart();
+    }, 2000);
   }
 
   trackScore();
 }
 
+/*
+ * If the cards don't match, set background color (via .clicked class)
+ *   - Remove transition, transform and bg-color on specific intervals
+ *   - Empty array of clicked cards
+ *   - Update score (moves + rating)
+ */
 function noMatch(array, item) {
-
-  console.log(array);
-
-  trackScore();
 
   $.each(array, function(index, value) {
     setTimeout(function() {
-      $('.clicked').css({'background-color': 'red'});
-      }, 450); 
+      $('.clicked').css({'background-color': 'red'})
+      .addClass('animated swing');
+      }, 600); 
   
     setTimeout(function() {
-      $('.'+value).parent().removeClass('open show')
+      $('.'+value).parent().removeClass('open show animated rotateIn swing')
       .css({'background-color': ''});
-      array.splice(0, 2); //emptys array
+      array.splice(0, 2);
       }, 1100);
 
     setTimeout(function() {
@@ -114,12 +126,19 @@ function noMatch(array, item) {
       .removeClass('clicked');
       }, 1600);
   });
+
+  trackScore();
 }
 
+/*
+ * When two .cards are clicked on, check to see if there is a match or not
+ *   - Remove transition, transform and bg-color on specific intervals
+ *   - Empty array of clicked cards
+ */
 function matchChecker(array, item) {
   array.push(item);
 
-  if (array.length === 2) { //run if two .cards have been clicked on
+  if (array.length === 2) {
     move++;
 
     if (array[0] === item) {
@@ -131,56 +150,95 @@ function matchChecker(array, item) {
   } 
 }
 
+/*
+ * Restart game
+ *   - Removes li items of .deck
+ *   - Shuffles and rebuilds deck .li
+ *   - Reset move, matchedCards, openCards and time counters
+ */
 function restart() {
+  console.log(matchedCards);
+  matchedCards = 0;
+
   $('.card').empty()
-  .removeClass('open show clicked match');
+  .removeClass('open show clicked match animated bounce');
 
   shuffle(cardOrder);
 
   $('.moves').html("");
 
-  time = new Date();
-
-  displayRating('.stars');
-
   if (move > 30) {
-    $(displayClass).append('<li><i class="fa fa-star"></i></li>');
-    $(displayClass).append('<li><i class="fa fa-star"></i></li>');
-    $(displayClass).append('<li><i class="fa fa-star"></i></li>');
-    alert("Game reset!");
+    $('.stars').append('<li><i class="fa fa-star"></i></li>');
+    $('.stars').append('<li><i class="fa fa-star"></i></li>');
+    $('.stars').append('<li><i class="fa fa-star"></i></li>');
 
   } else if (move > 20) {
-    $(displayClass).append('<li><i class="fa fa-star"></i></li>');
-    $(displayClass).append('<li><i class="fa fa-star"></i></li>');
-    alert("Game reset!");
+    $('.stars').append('<li><i class="fa fa-star"></i></li>');
+    $('.stars').append('<li><i class="fa fa-star"></i></li>');
 
   } else if (move > 10 ) {
-    $(displayClass).append('<li><i class="fa fa-star"></i></li>');
-    alert("Game reset!");
-
-  } else {
-    alert("Game reset!");
+    $('.stars').append('<li><i class="fa fa-star"></i></li>');
   }
 
   move = 0;
+  time = new Date();
+  openCards.splice(0, 2);
 }
 
-//Click event for checking cards
+/*
+ * Produce the modal overlay that displays final score, time and play again button
+ *   - Add HTML for modal overlay
+ *   - Display time and rating
+Sweet Alert from https://sweetalert.js.org/guides/#getting-started/
+*/
+function addModal() {
+  var modalDuration = duration;
+
+  swal({
+    title: 'Nice Work!',
+    text: 'You matched all of the cards.',
+    content: '.overlay-text-rating',
+    icon: 'success',
+    button: 'Play Again',
+  });
+
+  $('.swal-text').append('<h1 class= "overlay-text time">Completion Time: '+ modalDuration + ' Seconds</h1>');
+  $('.swal-text').append('<h1 class= "overlay-text rating">Your Rating:  </h1>');
+
+  $('.fa-star').each(function() {
+    $('.rating').append('<span><i class="fa fa-star"></i></span>');
+  });
+
+}
+
+/*
+ * Event .card click
+ *   - Add classes .open, .show, .clicked
+ *   - Add rotate transformation with transition
+ */
 var cardFlip = function(target) {
-  $(target).addClass('open show clicked')
-  .css({'transition': '400ms linear all', 'transform': 'rotateY(.5turn)'});
+  $(target).addClass('open show clicked animated rotateIn')
+  .css({'transition': '400ms linear all'});
 };
 
+/*
+ * When the document is ready, call the following:
+ *   - Shuffle function for randomizing card order
+ *   - On the first click within .deck, start the time counter
+ *   - event listener for .cards:
+ *      - Only occurs if the card isn't already in the openCards array and only when the array is < 2
+ *      - Check for a match or not
+ *      - Check the interval between time start and current time, rounded to whole number
+ *   - Event listeners for restart icon (body) and play again button (modal)
+ */
 $(document).ready(function() {
- //shuffle the card
+
   shuffle(cardOrder);
 
-  //start the timer on the first click only
   $('.deck').one('click', function() {
     time = new Date();
   });
 
-  //event listener for clicking on a card
   $('.card').click(function() {
 
     if ($(this).hasClass('clicked') === false && openCards.length < 2) {
@@ -191,34 +249,20 @@ $(document).ready(function() {
       matchChecker(openCards, classClicked);
 
     } else {
-      return;
+      return false;
     }
 
     setInterval(function() {
-      duration = Math.ceil(new Date() - time) / 1000;
+      duration = Math.ceil((new Date() - time) / 1000);
       }, 1000);
-
   });
 
-  //Event listener for restart
   $('.restart').click(function() {
-    restart();
-  });
+   restart();
 
-  $('.play-again').click(function() {
-    restart();
-  });
-
+   swal({
+      title: "Game Reset",
+      icon: "warning",
+    });
+ });
 });
-
-
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
